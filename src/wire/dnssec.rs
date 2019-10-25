12a17157b44b9a9930d9b76aae8a02ad6892015c
luozijun <luozijun.assistant@gmail.com>
2019-10-25 16:09:39 +0800

    Update src/wire/dnssec.rs

--- src/wire/dnssec.rs
+++ src/wire/dnssec.rs
@@ -133,10 +133,8 @@ impl std::fmt::Display for DNSKEYProtocol {
     }
 }
 
-// DNS Security Algorithm Numbers
-// https://www.iana.org/assignments/dns-sec-alg-numbers/dns-sec-alg-numbers.xhtml#dns-sec-alg-numbers-1
-// 
-// [RFC4034] [RFC3755] [RFC6014] [RFC6944]
+// Elliptic Curve Digital Signature Algorithm (DSA) for DNSSEC
+// https://tools.ietf.org/html/rfc6605
 // 
 // The KEY, SIG, DNSKEY, RRSIG, DS, and CERT RRs use an 8-bit number used
 // to identify the security algorithm being used.
@@ -150,73 +148,68 @@ impl std::fmt::Display for DNSKEYProtocol {
 // * There has been no determination of standardization of the use of this
 // algorithm with Transaction Security.
 // 
-// Number   Description                     Mnemonic            ZoneSigning  Trans.Sec.    Reference 
-// 0        Delete DS                       DELETE              N               N          [RFC4034] [RFC4398] [RFC8078]
-// 1        RSA/MD5 (deprecated, see 5)     RSAMD5              N               Y          [RFC3110] [RFC4034]
-// 2        Diffie-Hellman                  DH                  N               Y          [RFC2539] [proposed standard]
-// 3        DSA/SHA1                        DSA                 Y               Y          [RFC3755] [proposed standard]
-//                                                                                         [RFC2536] [proposed standard]
-//                                                Federal Information Processing Standards Publication (FIPS PUB) 186, 
-//                                                Digital Signature Standard, 18 May 1994.
-//                                                Federal Information Processing Standards Publication (FIPS PUB) 180-1,
-//                                                Secure Hash Standard, 17 April 1995. 
-//                                                (Supersedes FIPS PUB 180 dated 11 May 1993.)
-// 4        Reserved                                                                       [RFC6725]
-// 5        RSA/SHA-1                       RSASHA1             Y               Y          [RFC3110] [RFC4034]
-// 6        DSA-NSEC3-SHA1                  DSA-NSEC3-SHA1      Y               Y          [RFC5155] [proposed standard]
-// 7        RSASHA1-NSEC3-SHA1              RSASHA1-NSEC3-SHA1  Y               Y          [RFC5155] [proposed standard]
-// 8        RSA/SHA-256                     RSASHA256           Y               *          [RFC5702] [proposed standard]
-// 9        Reserved                                                                       [RFC6725]
-// 10       RSA/SHA-512                     RSASHA512           Y               *          [RFC5702] [proposed standard]
-// 11       Reserved                                                                       [RFC6725]
-// 12       GOST R 34.10-2001               ECC-GOST            Y               *          [RFC5933] [standards track]
-// 13       ECDSA Curve P-256 with SHA-256  ECDSAP256SHA256     Y               *          [RFC6605] [standards track]
-// 14       ECDSA Curve P-384 with SHA-384  ECDSAP384SHA384     Y               *          [RFC6605] [standards track]
-// 15       Ed25519                         ED25519             Y               *          [RFC8080] [standards track]
-// 16       Ed448                           ED448               Y               *          [RFC8080] [standards track]
-// 17-122   Unassigned
-// 123-251  Reserved                                                                       [RFC4034] [RFC6014]
-// 252      Reserved for Indirect Keys      INDIRECT            N               N          [RFC4034] [proposed standard]
-// 253      private algorithm               PRIVATEDNS          Y               Y          [RFC4034]
-// 254      private algorithm OID           PRIVATEOID          Y               Y          [RFC4034]
-// 255      Reserved                                                                       [RFC4034] [proposed standard]
-// 
-// 算法推荐
-//    +--------+--------------------+-----------------+-------------------+
-//    | Number | Mnemonics          | DNSSEC Signing  | DNSSEC Validation |
-//    +--------+--------------------+-----------------+-------------------+
-//    | 1      | RSAMD5             | MUST NOT        | MUST NOT          |
-//    | 3      | DSA                | MUST NOT        | MUST NOT          |
-//    | 5      | RSASHA1            | NOT RECOMMENDED | MUST              |
-//    | 6      | DSA-NSEC3-SHA1     | MUST NOT        | MUST NOT          |
-//    | 7      | RSASHA1-NSEC3-SHA1 | NOT RECOMMENDED | MUST              |
-//    | 8      | RSASHA256          | MUST            | MUST              |
-//    | 10     | RSASHA512          | NOT RECOMMENDED | MUST              |
-//    | 12     | ECC-GOST           | MUST NOT        | MAY               |
-//    | 13     | ECDSAP256SHA256    | MUST            | MUST              |
-//    | 14     | ECDSAP384SHA384    | MAY             | RECOMMENDED       |
-//    | 15     | ED25519            | RECOMMENDED     | RECOMMENDED       |
-//    | 16     | ED448              | MAY             | RECOMMENDED       |
-//    +--------+--------------------+-----------------+-------------------+
-// 
 // sha1            --> sha1::Sha1
 // sha256          --> sha2::Sha256
 // sha512          --> sha2::Sha512
-// ECDSAP256SHA256 --> 
-// ECDSAP384SHA384 --> 
-// ED25519         --> 
-// 
-// Ed25519 是一个使用SHA512/256和Curve25519的EdDSA签名算法
-// 
-// Elliptic Curve Digital Signature Algorithm (DSA) for DNSSEC
-// https://tools.ietf.org/html/rfc6605
 // 
 // https://csrc.nist.gov/csrc/media/publications/fips/186/3/archive/2009-06-25/documents/fips_186-3.pdf
-
+// 
 #[derive(PartialEq, Eq, Hash, Clone, Copy)]
 pub struct Algorithm(pub u8);
 
 impl Algorithm {
+    // DNS Security Algorithm Numbers
+    // https://www.iana.org/assignments/dns-sec-alg-numbers/dns-sec-alg-numbers.xhtml#dns-sec-alg-numbers-1
+    // 
+    // [RFC4034] [RFC3755] [RFC6014] [RFC6944]
+    // Number   Description                     Mnemonic            ZoneSigning  Trans.Sec.    Reference 
+    // 0        Delete DS                       DELETE              N               N          [RFC4034] [RFC4398] [RFC8078]
+    // 1        RSA/MD5 (deprecated, see 5)     RSAMD5              N               Y          [RFC3110] [RFC4034]
+    // 2        Diffie-Hellman                  DH                  N               Y          [RFC2539] [proposed standard]
+    // 3        DSA/SHA1                        DSA                 Y               Y          [RFC3755] [proposed standard]
+    //                                                                                         [RFC2536] [proposed standard]
+    //                                                Federal Information Processing Standards Publication (FIPS PUB) 186, 
+    //                                                Digital Signature Standard, 18 May 1994.
+    //                                                Federal Information Processing Standards Publication (FIPS PUB) 180-1,
+    //                                                Secure Hash Standard, 17 April 1995. 
+    //                                                (Supersedes FIPS PUB 180 dated 11 May 1993.)
+    // 4        Reserved                                                                       [RFC6725]
+    // 5        RSA/SHA-1                       RSASHA1             Y               Y          [RFC3110] [RFC4034]
+    // 6        DSA-NSEC3-SHA1                  DSA-NSEC3-SHA1      Y               Y          [RFC5155] [proposed standard]
+    // 7        RSASHA1-NSEC3-SHA1              RSASHA1-NSEC3-SHA1  Y               Y          [RFC5155] [proposed standard]
+    // 8        RSA/SHA-256                     RSASHA256           Y               *          [RFC5702] [proposed standard]
+    // 9        Reserved                                                                       [RFC6725]
+    // 10       RSA/SHA-512                     RSASHA512           Y               *          [RFC5702] [proposed standard]
+    // 11       Reserved                                                                       [RFC6725]
+    // 12       GOST R 34.10-2001               ECC-GOST            Y               *          [RFC5933] [standards track]
+    // 13       ECDSA Curve P-256 with SHA-256  ECDSAP256SHA256     Y               *          [RFC6605] [standards track]
+    // 14       ECDSA Curve P-384 with SHA-384  ECDSAP384SHA384     Y               *          [RFC6605] [standards track]
+    // 15       Ed25519                         ED25519             Y               *          [RFC8080] [standards track]
+    // 16       Ed448                           ED448               Y               *          [RFC8080] [standards track]
+    // 17-122   Unassigned
+    // 123-251  Reserved                                                                       [RFC4034] [RFC6014]
+    // 252      Reserved for Indirect Keys      INDIRECT            N               N          [RFC4034] [proposed standard]
+    // 253      private algorithm               PRIVATEDNS          Y               Y          [RFC4034]
+    // 254      private algorithm OID           PRIVATEOID          Y               Y          [RFC4034]
+    // 255      Reserved                                                                       [RFC4034] [proposed standard]
+    // 
+    // +--------+--------------------+-----------------+-------------------+
+    // | Number | Mnemonics          | DNSSEC Signing  | DNSSEC Validation |
+    // +--------+--------------------+-----------------+-------------------+
+    // | 1      | RSAMD5             | MUST NOT        | MUST NOT          |
+    // | 3      | DSA                | MUST NOT        | MUST NOT          |
+    // | 5      | RSASHA1            | NOT RECOMMENDED | MUST              |
+    // | 6      | DSA-NSEC3-SHA1     | MUST NOT        | MUST NOT          |
+    // | 7      | RSASHA1-NSEC3-SHA1 | NOT RECOMMENDED | MUST              |
+    // | 8      | RSASHA256          | MUST            | MUST              |
+    // | 10     | RSASHA512          | NOT RECOMMENDED | MUST              |
+    // | 12     | ECC-GOST           | MUST NOT        | MAY               |
+    // | 13     | ECDSAP256SHA256    | MUST            | MUST              |
+    // | 14     | ECDSAP384SHA384    | MAY             | RECOMMENDED       |
+    // | 15     | ED25519            | RECOMMENDED     | RECOMMENDED       |
+    // | 16     | ED448              | MAY             | RECOMMENDED       |
+    // +--------+--------------------+-----------------+-------------------+
+    // 
     pub const RSAMD5: Self             = Self(1);   // deprecated
     pub const DSA: Self                = Self(3);   // deprecated
     pub const RSASHA1: Self            = Self(5);   // MUST, 因为部署广泛, 需兼容
@@ -344,17 +337,37 @@ impl std::fmt::Display for Algorithm {
 pub struct DigestKind(pub u8);
 
 impl DigestKind {
-    pub const SHA1: Self    = Self(1); // 20 bytes
-    pub const SHA256: Self  = Self(2); // 32 bytes
+    // Delegation Signer (DS) Resource Record (RR) Type Digest Algorithms
+    // https://www.iana.org/assignments/ds-rr-types/ds-rr-types.xhtml
+    // 
+    // Value    Description     Status      Reference
+    // 0        Reserved        -           [RFC3658]
+    // 1        SHA-1           MANDATORY   [RFC3658]
+    // 2        SHA-256         MANDATORY   [RFC4509]
+    // 3        GOST R 34.11-94 OPTIONAL    [RFC5933]
+    // 4        SHA-384         OPTIONAL    [RFC6605]
+    // 5-255    Unassigned      -
+    pub const SHA1: Self            = Self(1); // 20 bytes
+    pub const SHA256: Self          = Self(2); // 32 bytes
+    pub const GOST_R_34_11_94: Self = Self(3);
+    pub const SHA384: Self          = Self(4);
 }
 
 impl DigestKind {
     #[inline]
     pub fn is_unassigned(&self) -> bool {
-        // 0
-        // 3 .. 255
+        // 5-255    Unassigned      -
         match self.0 {
-            0 | 3 ..= 255 => true,
+            5 ..= 255 => true,
+            _ => false,
+        }
+    }
+
+    #[inline]
+    pub fn is_reserved(&self) -> bool {
+        // 0        Reserved        -           [RFC3658]
+        match self.0 {
+            0 => true,
             _ => false,
         }
     }
@@ -365,13 +378,17 @@ impl std::fmt::Debug for DigestKind {
         match *self {
             Self::SHA1 => write!(f, "SHA1"),
             Self::SHA256 => write!(f, "SHA256"),
+            Self::GOST_R_34_11_94 => write!(f, "GOST-R-34_11-94"),
+            Self::SHA384 => write!(f, "SHA384"),
             _ => {
                 if self.is_unassigned() {
                     write!(f, "Unassigned({})", self.0)
+                } else if self.is_reserved() {
+                    write!(f, "Reserved({})", self.0)
                 } else {
                     write!(f, "Unknow({})", self.0)
                 }
-            },
+            }
         }
     }
 }
@@ -381,38 +398,3 @@ impl std::fmt::Display for DigestKind {
         write!(f, "{:?}", self)
     }
 }
-
-pub fn verify() {
-
-}
-
-// DNS KEY Record Diffie-Hellman Prime Lengths
-// https://www.iana.org/assignments/dns-sec-alg-numbers/dns-sec-alg-numbers.xhtml#prime-lengths
-// 
-// [RFC2539]
-// 
-// Value    Description                     Reference 
-// 0        Unassigned
-// 1        index into well-known table     [RFC2539]
-// 2        index into well-known table     [RFC2539]
-// 3-15     Unassigned
-// 
-
-
-// DNS KEY Record Diffie-Hellman Well-Known Prime/Generator Pairs
-// https://www.iana.org/assignments/dns-sec-alg-numbers/dns-sec-alg-numbers.xhtml#prime-generator-pairs
-// 
-// [RFC2539]
-// 
-// Range            Registration Procedures
-// 0x0000-0x07ff    Standards Action
-// 0x0800-0xbfff    RFC Required
-// 
-// Value            Description                             Reference
-// 0x0000           Unassigned
-// 0x0001           Well-Known Group 1: A 768 bit prime     [RFC2539]
-// 0x0002           Well-Known Group 2: A 1024 bit prime    [RFC2539]
-// 0x0003-0xbfff    Unassigned
-// 0xc000-0xffff    Private Use                             [RFC2539]
-// 
-
